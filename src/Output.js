00c39b57bae0fab171e1b94aa15689c6b2f6417a
Kenji Nakamura <dragondev1017@gmail.com>
2019-03-08 01:56:49 -0500

    Compose output into its own React component and Take out unused constructor

--- src/Output.js
+++ src/Output.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class Output extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div>
