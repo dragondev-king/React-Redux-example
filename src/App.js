ba0aabbbbe137f1b287265fbe9c2c407e4f28e01
Kenji Nakamura <dragondev1017@gmail.com>
2019-03-08 01:32:08 -0500

    Initialize Repo with Simple React Example

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+const canUseDOM = !!(
+  (typeof window !== 'undefined' &&
+    window.document && window.document.createElement)
+);
+if (canUseDOM) {
+  require('./App.css');
+}
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      'inputValue': '',
+      'submittedValue': 'Nothing has been submitted yet'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ submittedValue: this.state.inputValue });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="Input">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Input: <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <div className="Output">
+          Output: {this.state.submittedValue}
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
