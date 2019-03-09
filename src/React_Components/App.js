af0171c78fe3e045e6133e42e9c8f0bf52d65e4b
dragondev-king <dragondev1017@gmail.com>
2019-03-09 18:17:29 -0500

    Show how to use parent props and redux together

--- src/React_Components/App.js
+++ src/React_Components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Input />
+        <Input defaultInput={'Hello World!'}/>
         <Output />
       </div>
     );
