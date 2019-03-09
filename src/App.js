3e1f204a7f25794e490e141fd70d03d9a7765d74
Kenji Nakamura <dragondev1017@gmail.com>
2019-03-09 16:06:54 -0500

    Connect input component to redux for submitting values

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Input from './Input';
 import Output from './Output';
 import { connect } from 'react-redux';
-import { submitValue } from './actions/submittedValueActions';
 
 const canUseDOM = !!(
   (typeof window !== 'undefined' &&
@@ -13,32 +12,10 @@ if (canUseDOM) {
 }
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      'inputValue': '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ inputValue: event.target.value });
-  }
-
-  handleSubmit(event) {
-    this.props.onSubmitValue(this.state.inputValue);
-    event.preventDefault();
-  }
-
   render() {
-    console.log(this.props);
-
     return (
       <div className="App">
-        <Input inputValue={this.state.inputValue} handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+        <Input />
         <Output submittedValue={this.props.submittedValue} />
       </div>
     );
@@ -49,8 +26,4 @@ const mapStateToProps = state => {
   return state;
 }
 
-const mapActionsToProps = {
-  onSubmitValue: submitValue
-}
-
-export default connect(mapStateToProps, mapActionsToProps)(App);
+export default connect(mapStateToProps, null)(App);
--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { submitValue } from './actions/submittedValueActions';
 
 class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'inputValue': '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ inputValue: event.target.value });
+    }
+
+    handleSubmit(event) {
+        this.props.onSubmitValue(this.state.inputValue);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <label>
-                        Input: <input type="text" value={this.props.inputValue} onChange={this.props.handleChange} />
+                        Input: <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
@@ -15,4 +36,8 @@ class Input extends Component {
     }
 }
 
-export default Input;
+const mapActionsToProps = {
+    onSubmitValue: submitValue
+}
+
+export default connect(null, mapActionsToProps)(Input);
