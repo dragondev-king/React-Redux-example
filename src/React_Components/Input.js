47e9fc0f48e7db0f071604d0d06c86c20ac823b2
Kenji Nakamura <dragondev1017@gmail.com>
2019-03-09 18:17:29 -0500

    Show how to use parent props and redux together

--- src/React_Components/Input.js
+++ src/React_Components/Input.js
@@ -9,11 +9,12 @@ class Input extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            'inputValue': '',
+            'inputValue': this.props.defaultInput,
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
 
     handleChange(event) {
@@ -25,25 +26,48 @@ class Input extends Component {
         event.preventDefault();
     }
 
+    handleReset(event) {
+        this.setState({ inputValue: ''});
+        event.preventDefault();
+    }
+
+
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
                     <label>
                         Input: <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Submit" />
+                    <input type="reset" value="Clear" />
                 </form>
             </div>
         );
     }
 }
 
+// state is the state from Redux Store
+// props is the props from the parent component (which is App.js in this case)
+const mapStateToProps = (state, props) => {
+    return {
+        defaultInput: props.defaultInput + ' ' + state.submittedValue
+    };
+};
+
 // onSubmitValue is used to avoid naming conflicts with submitValue
 const mapActionsToProps = {
     onSubmitValue: submitValue
 }
 
-// The first value would be mapStateToProps, if mapStateToProps was needed.
+// Connecting mapStateToProps lets this component use this.props to get state from the Redux Store and props from the parent component.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// this.props can actually be used normally to get props from the parent, without mapStateToProps.
+// However, mapStateToProps can be used to avoid naming conflicts 
+// (Example: Imagine defaultInput is a prop on both the Redux Store and the parent. To fix, return defaultInputParent and defaultInputReduxStore in mapStateToProps).
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// By default, the Redux Store takes priority over parent props in this.props.
+// If Redux state is not needed, this code can be used: connect(null, mapActionsToProps)(Input)
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Connecting mapActionsToProps lets this component use actions defined in ../Redux/actions from this.props
-export default connect(null, mapActionsToProps)(Input);
+export default connect(mapStateToProps, mapActionsToProps)(Input);
