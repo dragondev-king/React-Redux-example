29da64f18cca0b5e5539bc26a6f79e11c2d276d9
dragondev-king <dragondev1017@gmail.com>
2019-03-09 21:19:50 -0500

    Add comment about 'action creators' and 'action' for clarity

--- src/Redux/actions/submittedValueActions.js
+++ src/Redux/actions/submittedValueActions.js
@@ -1,5 +1,7 @@
 export const SUBMIT_VALUE = 'SUBMIT_VALUE';
 
+// The function, submitValue, is an 'action creator'
+// The return value is an 'action'
 export function submitValue(input) {
     return {
         type: SUBMIT_VALUE,
